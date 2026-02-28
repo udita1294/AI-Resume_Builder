b16b7a7d611c9cd0da76587609d7d06cc15c6cb9
Udita Singh <udita1294@gmail.com>
2026-03-01 00:23:48 +0530

    login controller created

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -45,7 +45,46 @@ async function registerUserController(req, res) {
     }
 }
 
+async function loginUserController(req, res) {
+    try{
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ message : "All fields are required" });
+        }
+
+        const user = await userModel.findOne({ email });
+        if(!user){
+            return res.status(400).json({ message : "Invalid email or password" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({ message : "Invalid email or password" });
+        }
+
+        const token = jwt.sign(
+            {  id : user._id ,username : user.username,},
+             process.env.JWT_SECRET, { expiresIn : '1d' });
+        
+        res.cookie('token', token)
+
+        res.status(200).json({ message : "User logged in successfully", 
+            user : {
+                id : user._id,
+                username : user.username,
+                email : user.email,
+            },token
+         });
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message : "Server Error" });
+    }
+}
+
 
 module.exports = {
     registerUserController,
+    loginUserController,
 };
