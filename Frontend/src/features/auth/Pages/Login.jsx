521b8272bc20e5d3c387678673cb23556b37d650
Udita Singh <udita1294@gmail.com>
2026-03-01 16:45:41 +0530

    cors added

--- Frontend/src/features/auth/Pages/Login.jsx
+++ Frontend/src/features/auth/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { useNavigate, Link } from 'react-router'
 import "../auth.form.scss"
-// import { useAuth } from '../hooks/useAuth'
+import { useAuth } from '../hooks/useAuth'
 
 const Login = () => {
 
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,15 +1,63 @@
-import { useContext } from "react";
-import { AuthContext } from "../auth.context.jsx";
-import {login,register,logout,getMe} from "../services/auth.api.js"
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    const {user,setUser,loading,setLoading} = context;
 
-    const handleLogin = async ({email,password}) => {
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
+
+
+    const handleLogin = async ({ email, password }) => {
         setLoading(true)
-        const data = await login({email,password})
-        setUser(data.user)
-        setLoading(false)
+        try {
+            const data = await login({ email, password })
+            setUser(data.user)
+        } catch (err) {
+
+        } finally {
+            setLoading(false)
+        }
     }
+
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true)
+        try {
+            const data = await register({ username, email, password })
+            setUser(data.user)
+        } catch (err) {
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        try {
+            const data = await logout()
+            setUser(null)
+        } catch (err) {
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+
+        const getAndSetUser = async () => {
+            try {
+                const data = await getMe()
+                setUser(data.user)
+            } catch (err) { } finally {
+                setLoading(false)
+            }
+        }
+        getAndSetUser()
+    }, [])
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
 }
