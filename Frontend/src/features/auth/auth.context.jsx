dc4cda26a87cc78f05e2b9372f9da121884c6e90
Udita Singh <udita1294@gmail.com>
2026-03-01 17:09:08 +0530

    protected.jsx added

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -7,15 +7,6 @@ export const AuthProvider = ({ children }) => {
     const[user,setUser] = useState(null)
     const[loading,setLoading] = useState(false)
 
-    useEffect(() => {
-        const getAndSetUser = async () => {
-            const data = await getMe()
-            setUser(data.user)
-            setLoading(false)
-        }
-        getAndSetUser()
-    },[])
-
     return(
         <AuthContext.Provider value={{user,setUser,loading,setLoading}}>
             {children}
